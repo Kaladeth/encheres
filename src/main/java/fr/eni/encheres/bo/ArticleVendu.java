2ab1cb499d5f8bf83f6aebabb21adf7758205b4a
ejardin2022 <ejardin2022@105SE32-DQ0TVD3.ad.campus-eni.fr>
2022-08-16 16:37:50 +0200

    Création d'ArticleVendu + classes liées

---
+++ src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,168 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private String etatVente;
+	private Utilisateur utilisateur;
+	private List<Enchere> encheres;
+	private Categorie categorie;
+	
+	// - - - - - - - - - - CONSTRUCTEURS - - - - - - - - - - 
+	
+	// CONSTRUCTEUR VIDE
+	public ArticleVendu() {
+		
+	}
+	
+	// CONSTRUCTEUR TOTAL
+	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+	LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente, Utilisateur utilisateur, 
+	List<Enchere> encheres, Categorie categorie) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.utilisateur = utilisateur;
+		this.encheres = encheres;
+		this.categorie = categorie;
+	}
+	
+	// CONSTRUCTEUR DANS LE NO ARTICLE
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente, Utilisateur utilisateur, 
+			List<Enchere> encheres, Categorie categorie) {
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.utilisateur = utilisateur;
+		this.encheres = encheres;
+		this.categorie = categorie;
+	}
+
+	
+	
+	// - - - - - - - - - - GETTERS & SETTERS - - - - - - - - - -
+	
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public String getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	public List<Enchere> getEncheres() {
+		return encheres;
+	}
+
+	public void setEncheres(List<Enchere> encheres) {
+		this.encheres = encheres;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	// - - - - - - - - - - METHODE TO STRING - - - - - - - - - - 
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente + ", utilisateur=" + utilisateur
+				+ ", encheres=" + encheres + ", categorie=" + categorie + "]";
+	}
+	
+	// - - - - - - - - - - ADDER POUR LA LISTE - - - - - - - - - - 
+	public void addEnchere(Enchere enchere) {
+		encheres.add(enchere);
+	}
+	
+}
+
+
+

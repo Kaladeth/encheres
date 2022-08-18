62dfb741e87199a85395fb5da4b59133d419d1b5
ssalem2022 <ssalem2022@105SE32-1T0TVD3.ad.campus-eni.fr>
2022-08-18 15:47:23 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/Kaladeth/encheres.git into
    main

--- src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
@@ -1,8 +1,10 @@
 package fr.eni.encheres.dal.jdbc;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 import fr.eni.encheres.bo.Utilisateur;
@@ -12,30 +14,53 @@ import fr.eni.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurDaoJdbcImpl implements UtilisateurDAO {
 	String VALIDATE_LOGIN ="SELECT * FROM utilisateurs where (pseudo=? OR email=?) AND mot_de_passe=?"; 
-
+	String SELECTID="select*from utilisateur where nutilisateur";
+	
 	public Utilisateur selectById(Utilisateur element) throws DALException {
-		// TODO Auto-generated method stub
+		try(Connection cnx = ConnectionProvider.getConnection()
+				){
+			Statement stmt =  cnx.createStatement();
+			
+            ResultSet rs = ((java.sql.Statement) stmt).executeQuery(SELECTID); 
+            while(rs.next()) {
+            Utilisateur	utilisateur = new Utilisateur();
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				utilisateur.setPseudo(rs.getString("pseudo"));
+				utilisateur.setNom(rs.getString("nom"));
+				utilisateur.setPrenom(rs.getString("prenom"));
+				utilisateur.setEmail(rs.getString("email"));
+				utilisateur.setTelephone(rs.getString("telephone"));
+				utilisateur.setRue(rs.getString("rue"));
+				utilisateur.setCodePostal(rs.getString("code_postal"));
+				utilisateur.setVille(rs.getString("ville"));
+            }
+		}
+			
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 		return null;
 	}
 
 	public List<Utilisateur> selectAll(Utilisateur element) throws DALException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	public void insert(Utilisateur element) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	public void update(Utilisateur element) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void delete(int element) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -61,11 +86,12 @@ public class UtilisateurDaoJdbcImpl implements UtilisateurDAO {
 				utilisateur.setRue(rs.getString("rue"));
 				utilisateur.setCodePostal(rs.getString("code_postal"));
 				utilisateur.setVille(rs.getString("ville"));
-				utilisateur.setCredit(rs.getInt("credit"));
+				
 				if(rs.getByte("administrateur") ==0) {
 					utilisateur.setAdministrateur(false);
 					}else {utilisateur.setAdministrateur(true);}
 				}
+			
 			}catch (Exception e) {
 			DALException ex = new DALException("Problème dans l'authentification de l'utilisateur", e);
 			throw ex;}

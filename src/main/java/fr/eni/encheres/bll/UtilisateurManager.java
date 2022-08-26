4db900462f5dfb55b2eb2adef4acd85c80f5c825
mshili2022 <mshili2022@105SE32-BR0TVD3.ad.campus-eni.fr>
2022-08-26 11:45:47 +0200

    qlq modification methode update user

--- src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateurManager.java
@@ -141,90 +141,83 @@ public class UtilisateurManager {
 	
 	
 	// * * * * * METHODE UPDATE * * * * *
-	public void updateUtilisateur(int idUser, String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String cp, String ville, String ancienMdp, String mdpActuel, String mdp, String confirmationMdp) throws BLLException {
-		BLLException bllExceptions = new BLLException();
-		
-		// VERIFICATION DES REGLES METIER
-				
-		if(pseudo == null || pseudo.isEmpty() || pseudo.isBlank()) {
-			Exception e = new Exception("L'identifiant est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(pseudo.indexOf('@') != -1) {
-			Exception e = new Exception("L'identifiant ne peut pas contenir d'arobase !");
-			bllExceptions.addException(e);
-		}
-		if(pseudo.matches("[a-zA-Z0-9]")) {
-			Exception e = new Exception("L'identifiant n�accepte que des caractères alphanumériques !");
-			bllExceptions.addException(e);
-		}
-		if(nom == null || nom.isEmpty() || nom.isBlank()) {
-			Exception e = new Exception("Le nom est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(prenom ==null || prenom.isEmpty() || nom.isBlank()) {
-			Exception e = new Exception("Le prénom est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(email == null|| email.isEmpty() || email.isBlank()) {
-			Exception e = new Exception("L'e-mail est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		// email au format XXX@XXX.XXX
+    public void updateUtilisateur(int idUser, String pseudo, String nom, String prenom, String email, String telephone, String rue,
+            String cp, String ville, String ancienMdp, String mdpActuel, String mdp, String confirmationMdp) throws BLLException {
+        BLLException bllExceptions = new BLLException();
+        // VERIFICATION DES REGLES METIER
+        if(pseudo == null || pseudo.isEmpty() || pseudo.isBlank()) {
+            Exception e = new Exception("L'identifiant est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(pseudo.indexOf('@') != -1) {
+            Exception e = new Exception("L'identifiant ne peut pas contenir d'arobase !");
+            bllExceptions.addException(e);
+        }
+        if(pseudo.matches("[a-zA-Z0-9]")) {
+            Exception e = new Exception("L'identifiant n�accepte que des caractères alphanumériques !");
+            bllExceptions.addException(e);
+        }
+        if(nom == null || nom.isEmpty() || nom.isBlank()) {
+            Exception e = new Exception("Le nom est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(prenom ==null || prenom.isEmpty() || nom.isBlank()) {
+            Exception e = new Exception("Le prénom est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(email == null|| email.isEmpty() || email.isBlank()) {
+            Exception e = new Exception("L'e-mail est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        // email au format XXX@XXX.XXX
         if (!Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$").matcher(email.toUpperCase()).matches()) {
-        	Exception e = new Exception("L'e-mail n'est pas au bon format !");
-			bllExceptions.addException(e);
+            Exception e = new Exception("L'e-mail n'est pas au bon format !");
+            bllExceptions.addException(e);
         }
         String telephoneCorrige = telephone;
-		if(telephone == null || telephone.isEmpty() || telephone.isBlank()) {
-			telephoneCorrige = "";
-			}
-		if(rue == null|| rue.isEmpty() || rue.isBlank()) {
-			Exception e = new Exception("La rue est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(cp == null|| cp.isEmpty() || cp.isBlank()) {
-			Exception e = new Exception("Le code postal est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(ville == null|| ville.isEmpty() || ville.isBlank()) {
-			Exception e = new Exception("La ville est obligatoire !");
-			bllExceptions.addException(e);
-		}
-		if(!mdp.equals(confirmationMdp)){
-			Exception e = new Exception("Les nouveaux mots de passe ne sont pas identiques pas !");
-			bllExceptions.addException(e);
-		}
-		String mdpATransmettre = null;
-		if(ville == null|| ville.isEmpty() || ville.isBlank()) {
-			mdpATransmettre = mdpActuel;
-		}else{
-			if(mdpActuel.equals(mdp)) {
-			Exception e = new Exception("Pour modifier le mot de passe, merci d'en saisir un nouveau !");
-			bllExceptions.addException(e);}
-			}
-		
-		if(!mdp.equals(ancienMdp)){
-			Exception e = new Exception("Le mot de passe est incorrect, la modification ne peut donc pas être faite !");
-			bllExceptions.addException(e);
-		}
-		
-		
-		if (!bllExceptions.isEmpty()) {
-			throw bllExceptions;
-		}
-		
-		// CREATION DE L'UTILISATEUR A ENVOYER EN BASE DE DONNEES
-		Utilisateur utilisateur = new Utilisateur(idUser, pseudo, nom, prenom, email, telephoneCorrige, rue, cp, ville, mdpATransmettre);
-		try {
-			utilisateurDao.update(utilisateur);
-						
-		} catch (DALException e) {
-			bllExceptions.addException(e);
-			throw bllExceptions;
-		}
-	}
+        if(telephone == null || telephone.isEmpty() || telephone.isBlank()) {
+            telephoneCorrige = "";
+            }
+        if(rue == null|| rue.isEmpty() || rue.isBlank()) {
+            Exception e = new Exception("La rue est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(cp == null|| cp.isEmpty() || cp.isBlank()) {
+            Exception e = new Exception("Le code postal est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(ville == null|| ville.isEmpty() || ville.isBlank()) {
+            Exception e = new Exception("La ville est obligatoire !");
+            bllExceptions.addException(e);
+        }
+        if(!mdp.equals(confirmationMdp)){
+            Exception e = new Exception("Les nouveaux mots de passe ne sont pas identiques pas !");
+            bllExceptions.addException(e);
+        }
+        if(!mdpActuel.equals(ancienMdp)){
+            Exception e = new Exception("Le mot de passe est incorrect, la modification ne peut donc pas être faite !");
+            bllExceptions.addException(e);
+        }
+        String mdpATransmettre = null;
+        if(mdp == null || mdp.isBlank()) {
+            mdpATransmettre = mdpActuel;
+        }else{
+            if(mdpActuel.equals(mdp)) {
+            Exception e = new Exception("Pour modifier le mot de passe, merci d'en saisir un nouveau !");
+            bllExceptions.addException(e);}
+            }
+        if (!bllExceptions.isEmpty()) {
+            throw bllExceptions;
+        }
+        // CREATION DE L'UTILISATEUR A ENVOYER EN BASE DE DONNEES
+        Utilisateur utilisateur = new Utilisateur(idUser, pseudo, nom, prenom, email, telephoneCorrige, rue, cp, ville, mdpATransmettre);
+        try {
+            utilisateurDao.update(utilisateur);
+        } catch (DALException e) {
+            bllExceptions.addException(e);
+            throw bllExceptions;
+        }
+    }
 	
 	// * * * * * METHODE SELECT BY ID * * * * *
 	
